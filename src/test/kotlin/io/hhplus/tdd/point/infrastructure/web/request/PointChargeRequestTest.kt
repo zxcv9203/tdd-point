e75f32dbe6d03f590bb0033f3cc4c0b4ea81b5ce
yongckim <zxcv9203@naver.com>
2024-09-23 20:18:24 +0900

    :recycle: refactor : 포인트 요청 객체 테스트시 @ParameterizedTest를 이용하여 경계값 테스트하도록 수정

--- src/test/kotlin/io/hhplus/tdd/point/infrastructure/web/request/PointChargeRequestTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/infrastructure/web/request/PointChargeRequestTest.kt
@@ -1,39 +1,32 @@
 package io.hhplus.tdd.point.infrastructure.web.request
 
 import io.hhplus.tdd.common.error.ErrorMessage
-import org.assertj.core.api.Assertions.assertThat
+import io.hhplus.tdd.point.domain.PointPolicy
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 @DisplayName("PointChargeRequest 테스트")
 class PointChargeRequestTest {
     @Nested
     @DisplayName("포인트 충전 요청 생성")
     inner class Init {
-        @Test
-        @DisplayName("최대 포인트 이하의 양수의 포인트가 주어지면 생성에 성공합니다.")
-        fun success() {
-            val want = 100L
-
-            val got = PointChargeRequest(want).amount
-
-            assertThat(got).isEqualTo(want)
-        }
-
-        @Test
+        @ParameterizedTest(name = "포인트 사용 요청이 {0}인 경우 생성에 실패합니다.")
+        @ValueSource(longs = [0L, -1L])
         @DisplayName("0 이하의 포인트가 주어지면 생성에 실패합니다.")
-        fun lessZeroAmount() {
-            assertThatThrownBy { PointChargeRequest(0) }
+        fun lessZeroAmount(amount: Long) {
+            assertThatThrownBy { PointChargeRequest(amount) }
                 .isInstanceOf(IllegalArgumentException::class.java)
                 .hasMessage(ErrorMessage.INVALID_POINT_AMOUNT.message)
         }
 
-        @Test
+        @ParameterizedTest(name = "포인트 사용 요청이 {0}인 경우 생성에 실패합니다.")
+        @ValueSource(longs = [PointPolicy.MAX_POINT, Long.MAX_VALUE])
         @DisplayName("최대 포인트 이상으로 포인트가 주어지면 생성에 실패합니다.")
-        fun moreThanMaxAmount() {
-            assertThatThrownBy { PointChargeRequest(Long.MAX_VALUE) }
+        fun moreThanMaxAmount(amount: Long) {
+            assertThatThrownBy { PointChargeRequest(amount) }
                 .isInstanceOf(IllegalArgumentException::class.java)
                 .hasMessage(ErrorMessage.MAX_POINT_EXCEEDED.message)
         }
