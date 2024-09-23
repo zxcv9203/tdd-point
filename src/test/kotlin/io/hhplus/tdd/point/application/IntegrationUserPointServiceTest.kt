b5b3680f0751a5ecd9e19c280b9df926917696a0
yongckim <zxcv9203@naver.com>
2024-09-23 20:26:52 +0900

    :white_check_mark: test : 동시에 포인트를 사용하는 경우에 대한 테스트 추가

--- src/test/kotlin/io/hhplus/tdd/point/application/IntegrationUserPointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/application/IntegrationUserPointServiceTest.kt
@@ -2,7 +2,9 @@ package io.hhplus.tdd.point.application
 
 import io.hhplus.tdd.helper.ConcurrentTestHelper
 import io.hhplus.tdd.point.infrastructure.web.request.PointChargeRequest
+import io.hhplus.tdd.point.infrastructure.web.request.PointUseRequest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -19,14 +21,40 @@ class IntegrationUserPointServiceTest(
     @DisplayName("포인트 충전")
     inner class Charge {
         @Test
-        @DisplayName("[성공] 동시에 여러 쓰레드가 접근하는 경우 포인트가 정상 저장된다.")
+        @DisplayName("동시에 여러 쓰레드가 접근하는 경우 포인트가 정상 충전된다.")
         fun success() {
-            val userId = 1L
+            val id = 1L
             val request = PointChargeRequest(100L)
             val want = 10000L
 
-            ConcurrentTestHelper.executeConcurrentTasks(99) { userPointService.charge(userId, request) }
-            val got = userPointService.charge(userId, request).point
+            ConcurrentTestHelper.executeConcurrentTasks(99) { userPointService.charge(id, request) }
+            val got = userPointService.charge(id, request).point
+
+            assertThat(got).isEqualTo(want)
+        }
+    }
+
+    @Nested
+    @DisplayName("포인트 사용")
+    inner class Use {
+        private val id = 2L
+
+        @BeforeEach
+        fun setUp() {
+            val chargeRequest = PointChargeRequest(10000L)
+            userPointService.charge(id, chargeRequest)
+        }
+
+        @Test
+        @DisplayName("동시에 여러 쓰레드가 접근하는 경우 포인트가 정상 사용된다.")
+        fun success() {
+            val useRequest = PointUseRequest(100L)
+            val want = 0L
+
+            ConcurrentTestHelper.executeConcurrentTasks(99) {
+                userPointService.use(id, useRequest)
+            }
+            val got = userPointService.use(id, useRequest).point
 
             assertThat(got).isEqualTo(want)
         }
