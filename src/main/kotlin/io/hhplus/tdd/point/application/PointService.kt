236bb386d96ae9e2d2e9dbe61bffdd6033c38082
yongckim <zxcv9203@naver.com>
2024-09-26 23:29:24 +0900

    :recycle: refactor : 포인트 충전 / 사용시 동시성 제어될 수 있도록 리팩토링

--- src/main/kotlin/io/hhplus/tdd/point/application/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/application/PointService.kt
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point.application
 
+import io.hhplus.tdd.common.LockManager
 import io.hhplus.tdd.point.domain.PointHistory
 import io.hhplus.tdd.point.domain.UserPoint
 import io.hhplus.tdd.point.infrastructure.web.request.PointChargeRequest
@@ -10,22 +11,27 @@ import org.springframework.stereotype.Service
 class PointService(
     private val pointHistoryService: PointHistoryService,
     private val userPointService: UserPointService,
+    private val lockManager: LockManager,
 ) {
     fun charge(
         id: Long,
         request: PointChargeRequest,
     ): UserPoint =
-        userPointService
-            .charge(id, request)
-            .also { pointHistoryService.save(PointHistory.createByCharge(id, request.amount)) }
+        lockManager.withLock {
+            userPointService
+                .charge(id, request)
+                .also { pointHistoryService.save(PointHistory.createByCharge(id, request.amount)) }
+        }
 
     fun use(
         id: Long,
         request: PointUseRequest,
     ): UserPoint =
-        userPointService
-            .use(id, request)
-            .also { pointHistoryService.save(PointHistory.createByUse(id, request.amount)) }
+        lockManager.withLock {
+            userPointService
+                .use(id, request)
+                .also { pointHistoryService.save(PointHistory.createByUse(id, request.amount)) }
+        }
 
     fun getById(id: Long): UserPoint = userPointService.getById(id)
 
